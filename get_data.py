5269df36cda0a812ebe8e84f201a889e06f6d0b4
Akram Guediri <71324128+akramguediri@users.noreply.github.com>
2023-11-10 15:31:58 +0100

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -1,11 +1,21 @@
 import requests
 import pandas as pd
+import datetime
+
+# Get the current timestamp (current date and time)
+current = datetime.datetime.now()
+
+# Calculate the timestamp for one year ago
+one_year = current - datetime.timedelta(days=365)
+
+# Print the timestamp for one year ago
+print(one_year.timestamp())
+
 
 apikey = '73f7585c1934e4530c64305b2f19c3ad'
 lat = '49.150002'
 lon = '9.216600'
-url = 'http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat='+lat+'&lon='+lon+'&appid='+apikey
-
+url = 'http://api.openweathermap.org/data/2.5/air_pollution/history?lat='+lat+'&lon='+lon+'&start='+str(int(one_year.timestamp()))+'&end='+str(int(current.timestamp()))+'&appid='+apikey
 response = requests.get(url)
 data = response.json()  # Use response.json() to parse the JSON data
 
@@ -31,3 +41,29 @@ df = pd.DataFrame(data_to_convert)
 # Specify the name of the CSV file
 csv_file = 'data.csv'
 df.to_csv(csv_file, index=False)
+import pandas as pd
+
+def clean_and_save_data(input_file, output_file):
+    # Load the CSV data into a DataFrame
+    df = pd.read_csv(input_file)
+
+    # Check for missing or corrupted data
+    # You can define your own criteria for identifying corrupted data
+    # For example, you can check for missing values, NaNs, or outliers
+    corrupted_rows = df.isnull().any(axis=1)
+
+    # Remove corrupted rows
+    cleaned_df = df[~corrupted_rows]
+
+    # Save the cleaned data to a new CSV file
+    cleaned_df.to_csv(output_file, index=False)
+
+    # Print some statistics about the cleaning process
+    num_removed = len(df) - len(cleaned_df)
+    print(f"Removed {num_removed} corrupted rows.")
+    print(f"Cleaned data saved to {output_file}.")
+
+# Example usage:
+input_file = 'data.csv'  # Replace with your input file path
+output_file = 'data.csv'  # Replace with your desired output file path
+clean_and_save_data(input_file, output_file)
--- modellearning.py
+++ modellearning.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -39,6 +40,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(X_train.shape[1],)),  # Input layer
     tf.keras.layers.Dense(64, activation='relu'),  # Hidden layer
+    tf.keras.layers.Dense(64, activation='sigmoid'),  # Hidden layer
     tf.keras.layers.Dense(y_train.shape[1])  # Output layer for multiple variables
 ])
 
@@ -46,7 +48,7 @@ model = tf.keras.Sequential([
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-model.fit(X_train, y_train, epochs=100, batch_size=32)
+model.fit(X_train, y_train, epochs=200, batch_size=32)
 
 # Make predictions for future timestamps
 future_features = np.array([
@@ -62,15 +64,43 @@ future_predictions = model.predict(future_features)
 # Create a DataFrame with the results
 results = pd.DataFrame({
     'Timestamp': ['2023-12-01T00:00:00'],
-    'Main_AQI': [future_predictions[0][0]],
-    'CO': [future_predictions[0][1]],
-    'NO': [future_predictions[0][2]],
-    'NO2': [future_predictions[0][3]],
-    'O3': [future_predictions[0][4]],
-    'SO2': [future_predictions[0][5]],
-    'PM2.5': [future_predictions[0][6]],
-    'PM10': [future_predictions[0][7]]
+    'Main_AQI': [abs(int(future_predictions[0][0]))],
+    'CO': [abs(future_predictions[0][1])],
+    'NO': [abs(future_predictions[0][2])],
+    'NO2': [abs(future_predictions[0][3])],
+    'O3': [abs(future_predictions[0][4])],
+    'SO2': [abs(future_predictions[0][5])],
+    'PM2.5': [abs(future_predictions[0][6])],
+    'PM10': [abs(future_predictions[0][7])]
 })
 
 # Save the results to a CSV file
 results.to_csv('predicted_results.csv', index=False)
+
+client = OpenAI(api_key="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")  # Replace 'YOUR_API_KEY_HERE' with your actual OpenAI API key
+
+def generate_report(results):
+    # Prepare context for the API
+    context = "The results of the predictions are as follows:\n"
+
+    # Adding each result to the context
+    for col, val in results.items():
+        context += f"{col}: {val}\n"
+
+    # Generate a report using ChatGPT
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": context + "Please summarize these results and provide insights.",
+            }
+        ],
+        model="gpt-3.5-turbo-instruct",  # Replace with your desired model
+    )
+
+    # Extract and return the generated report
+    return response.choices[0].message.content
+
+# Use the function with your results DataFrame
+report = generate_report(results)
+print(report)
